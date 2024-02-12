ded850400454b90ca6aba9f18a24eeb57b9e2949
lucca vinicios <vini.lucca42@gmail.com>
2024-02-11 22:39:43 -0300

    feat: implement validation from get strategies

--- scans/magic-scan.py
+++ scans/magic-scan.py
@@ -1,16 +1,37 @@
 import os
+import sys
 import argparse
 
 
-def scan_filers(path: str, file_strategy: str, export_result: str):
-    print(path, file_strategy, export_result)
+minimal_version = (3, 12)
+
+
+if sys.version_info < minimal_version:
+    raise RuntimeError("minimal version is not satisfied")
+
+
+FILE_STRATEGIES = {
+    "SQLITE": ...,
+}
+
+EXPORT_RESULT_STRATEGIES = {
+    "TXT": ...,
+    "SQLITE": ...,
+}
+
+def scan_filers(path: str, file_strategy: str | None, export_result: str):
+    global FILE_STRATEGIES
+    file_strategy_exists = FILE_STRATEGIES.get(file_strategy) is not None
+    assert file_strategy_exists, f"file strategy not found {file_strategy}. You can try {','.join(FILE_STRATEGIES.keys())}"
+    export_result_strategy_exists = EXPORT_RESULT_STRATEGIES.get(export_result) is not None
+    assert export_result_strategy_exists, f"export result strategy not found {export_result}. You can try {','.join(EXPORT_RESULT_STRATEGIES.keys())}"
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script to scan files, from magic bytes')
     parser.add_argument('path', metavar='PATH', type=str, nargs='?', default=os.getcwd(), help='Path to the directory to be scanned (default: current directory)')
     parser.add_argument('--file-strategy', metavar='FILE_STRATEGY', type=str, help='Strategy to scan the files: e.g.: SQLITE')
-    parser.add_argument('--export-result', metavar='EXPORT_RESULT', type=str, default="TXT", help='Export result e.g.: SQLITE, TXT')
+    parser.add_argument('--export-result', metavar='EXPORT_RESULT', type=str, default="TXT", help='Export result e.g.: SQLITE, TXT,  (default: TXT)')
     args = parser.parse_args()
 
 
