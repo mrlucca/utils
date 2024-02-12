935382adb6acd8d2e18b65b4e1f5a994180adc9c
lucca vinicios <vini.lucca42@gmail.com>
2024-02-12 00:17:43 -0300

    feat: implement export csv strategy

--- scans/magic-scan.py
+++ scans/magic-scan.py
@@ -2,24 +2,40 @@ import os
 import sys
 import time
 import argparse
-from typing import Tuple, Callable
+from typing import List, Self, Tuple, Callable, Protocol
 
 minimal_version = (3, 12)
+
+MAX_THREADS = 1
 
 if sys.version_info < minimal_version:
     raise RuntimeError("minimal version is not satisfied")
     
-FILE_STRATEGIES = {
-    "SQLITE": ( 
-        b"\x53\x51\x4c\x69\x74\x65\x20\x66\x6f\x72\x6d\x61\x74\x20\x33\x00",
-        16,
-    ),
-}
 
-EXPORT_RESULT_STRATEGIES = {
-    "TXT": ...,
-    "SQLITE": ...,
-}
+class ExportStrategy(Protocol):
+    def __enter__(self) -> Self: ...
+    def __exit__(self): ...
+    def push_result(self, result: tuple): ...
+
+
+class ExportCSVStrategy(ExportStrategy):
+    def __init__(self):
+        self._path = os.path.join(os.getcwd(), "export_result.csv")
+        self._file = None
+
+    def __enter__(self):
+        self._file = open(self._path, "w+")
+        self._file.write("current_path;size_in_mb;created_at;modified_at\n")
+        return self
+
+    def __exit__(self):
+        if self._file:
+            self._file.close()
+
+    def push_result(self, result: tuple):
+        assert self._file, "file does not exists"
+        self._file.write(f"{result[0]};{result[1]};{result[2]};{result[3]}\n")
+
 
 
 def factory_check_magic_bytes_from(magic_bytes: bytes, read_size: int) -> Callable[[str], bool]:
@@ -55,7 +71,6 @@ def get_file_info(path: str) -> Tuple[float, str, str]:
 def scan_filers(path: str, check_magic_bytes_cb: Callable[[str], bool]):
     for root, _, filers in os.walk(path):
         is_empty = len(filers) == 0
-        
         if is_empty:
             continue
 
@@ -66,13 +81,28 @@ def scan_filers(path: str, check_magic_bytes_cb: Callable[[str], bool]):
                 continue
             
             size_in_mb, created_at, modified_at = get_file_info(current_path)
-            print((
+
+            yield (
                 current_path,
                 size_in_mb,
                 created_at,
                 modified_at
-            ))
-            
+            )
+
+
+
+FILE_STRATEGIES = {
+    "SQLITE": ( 
+        b"\x53\x51\x4c\x69\x74\x65\x20\x66\x6f\x72\x6d\x61\x74\x20\x33\x00",
+        16,
+    ),
+}
+
+
+EXPORT_RESULT_STRATEGIES = {
+    "CSV": ExportCSVStrategy,
+    "SQLITE": ...,
+}         
 
 
 def setup_and_run_scan_filers(path: str, file_strategy: str | None, export_result: str):
@@ -80,18 +110,21 @@ def setup_and_run_scan_filers(path: str, file_strategy: str | None, export_resul
     file_strategy_resp = FILE_STRATEGIES.get(file_strategy)
     file_strategy_exists = file_strategy_resp is not None
     assert file_strategy_exists, f"file strategy not found {file_strategy}. You can try {','.join(FILE_STRATEGIES.keys())}"
-    export_result_strategy_resp = EXPORT_RESULT_STRATEGIES.get(export_result)
+    export_result_strategy_resp: ExportStrategy | None = EXPORT_RESULT_STRATEGIES.get(export_result)
     export_result_strategy_exists = export_result_strategy_resp is not None
     assert export_result_strategy_exists, f"export result strategy not found {export_result}. You can try {','.join(EXPORT_RESULT_STRATEGIES.keys())}"
     magic_bytes, read_size = file_strategy_resp
+
     check_magic_bytes_cb = factory_check_magic_bytes_from(magic_bytes, read_size)
-    scan_filers(path, check_magic_bytes_cb)
+    with export_result_strategy_resp() as strategy:
+        for result in scan_filers(path, check_magic_bytes_cb):
+            strategy.push_result(result)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script to scan files, from magic bytes')
     parser.add_argument('path', metavar='PATH', type=str, nargs='?', default=os.getcwd(), help='Path to the directory to be scanned (default: current directory)')
     parser.add_argument('--file-strategy', metavar='FILE_STRATEGY', type=str, help='Strategy to scan the files: e.g.: SQLITE')
-    parser.add_argument('--export-result', metavar='EXPORT_RESULT', type=str, default="TXT", help='Export result e.g.: SQLITE, TXT,  (default: TXT)')
+    parser.add_argument('--export-result', metavar='EXPORT_RESULT', type=str, default="CSV", help='Export result e.g.: SQLITE, CSV,  (default: CSV)')
     args = parser.parse_args()
     setup_and_run_scan_filers(args.path, args.file_strategy, args.export_result)
