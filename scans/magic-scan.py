27203fc5fad89c0c6735b8340b7cb3a3e0c01a18
lucca vinicios <vini.lucca42@gmail.com>
2024-02-11 23:07:51 -0300

    feat: implement scan filers

--- scans/magic-scan.py
+++ scans/magic-scan.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+from typing import Callable
 
 
 minimal_version = (3, 12)
@@ -23,27 +24,49 @@ EXPORT_RESULT_STRATEGIES = {
 }
 
 
-def check_magic_bytes_from(path: str, magic_bytes: bytes, read_size: int) -> str | None:
-    if not os.path.exists(path):
-        return
+def factory_check_magic_bytes_from(magic_bytes: bytes, read_size: int) -> Callable[[str], bool]:
     
-    try:
-        with open(path, 'rb') as file:
-            header = file.read(read_size) 
-    except (PermissionError, OSError):
-        # TODO add log
-        return False
-    else:
-        return header.startswith(magic_bytes)
+    def check_magic_bytes_callback(path: str) -> str | None:
+        if not os.path.exists(path):
+            return
+        
+        try:
+            with open(path, 'rb') as file:
+                header = file.read(read_size) 
+        except (PermissionError, OSError):
+            # TODO add log
+            return False
+        else:
+            return header.startswith(magic_bytes)
+        
+    return check_magic_bytes_callback
     
 
-def scan_filers(path: str, file_strategy: str | None, export_result: str):
+def scan_filers(path: str, check_magic_bytes_cb: Callable[[str], bool]):
+    for root, _, filers in os.walk(path):
+        is_empty = len(filers) == 0
+        
+        if is_empty:
+            continue
+
+        for file_path in filers:
+            current_path = os.path.join(root, file_path)
+
+            if not check_magic_bytes_cb(current_path):
+                continue
+            
+            print(current_path)
+
+
+def setup_and_run_scan_filers(path: str, file_strategy: str | None, export_result: str):
     global FILE_STRATEGIES
     file_strategy_exists = FILE_STRATEGIES.get(file_strategy) is not None
     assert file_strategy_exists, f"file strategy not found {file_strategy}. You can try {','.join(FILE_STRATEGIES.keys())}"
     export_result_strategy_exists = EXPORT_RESULT_STRATEGIES.get(export_result) is not None
     assert export_result_strategy_exists, f"export result strategy not found {export_result}. You can try {','.join(EXPORT_RESULT_STRATEGIES.keys())}"
-
+    magic_bytes, read_size = file_strategy_exists
+    check_magic_bytes_cb = factory_check_magic_bytes_from(magic_bytes, read_size)
+    scan_filers(path, check_magic_bytes_cb)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script to scan files, from magic bytes')
@@ -51,6 +74,4 @@ if __name__ == '__main__':
     parser.add_argument('--file-strategy', metavar='FILE_STRATEGY', type=str, help='Strategy to scan the files: e.g.: SQLITE')
     parser.add_argument('--export-result', metavar='EXPORT_RESULT', type=str, default="TXT", help='Export result e.g.: SQLITE, TXT,  (default: TXT)')
     args = parser.parse_args()
-
-
-    scan_filers(args.path, args.file_strategy, args.export_result)
+    setup_and_run_scan_filers(args.path, args.file_strategy, args.export_result)
